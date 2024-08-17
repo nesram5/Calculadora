c3446e446d3cc1b731823b41773355ec7ea56c0e
Nestor Ramirez <49355265+nesram5@users.noreply.github.com>
2024-08-17 13:26:19 -0400

    Add function Borrar Historial. Optimized code

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,165 +1,90 @@
+//Object Calculator
 const Calculator = {
     current: '',
     previous: '',
-    operator: '',    
-    bttn: document.querySelectorAll('button'),
+    operator: '',
+    display: document.getElementById('result'),    
+    buttons: document.querySelectorAll('button'),
 
-    Operation: () => {
+    handleButtonClick: (element) => {
+        const value = element.innerHTML;
+        
+        if (value === 'Limpiar Historial'){
+                Calculator.clearHistory();
+        }
+        else if(element.classList.contains('number') ) {
+                Calculator.current += value;
+                Calculator.updateDisplay();
+        }
+        else if(value === '=') {     
+                if (Calculator.previous && Calculator.operator && Calculator.current){
+                    let prev = Calculator.current;
+                    Calculator.current = Calculator.handleOperation();
+                    let result = Calculator.current;
+                    Calculator.addToHistory(prev,result);
+                    Calculator.updateDisplay();
+                }
+        }
+        else if(value === 'C'){
+                Calculator.clearAll();
+        }
+        else {
+            Calculator.prepareOperation(element);
+        }
+          
+     },
+
+    handleOperation: () => {
         if (Calculator.previous != '' && Calculator.operator != '' && Calculator.current != ''){
+            const prev = parseFloat(Calculator.previous);
+            const curr = parseFloat(Calculator.current);
             switch (Calculator.operator) {
-                case '+':
-                    return parseFloat(Calculator.previous) + parseFloat(Calculator.current);
-                case '-':
-                    return parseFloat(Calculator.previous) - parseFloat(Calculator.current);
-                case 'x':
-                    return parseFloat(Calculator.previous) * parseFloat(Calculator.current);                    
-                case '/':
-                    return parseFloat(Calculator.previous) / parseFloat(Calculator.current);
-                default:
-                    return;
+                case '+': return prev + curr;
+                case '-': return prev - curr;
+                case 'x': return prev * curr;
+                case '/': return curr !== 0 ? prev / curr : 'Error';
+                default: return curr;
             } 
         }           
     },
 
-    SelectOperation: (element) => {
+    prepareOperation: (element) => {
         if (Calculator.current != ''){
             Calculator.previous = Calculator.current;
         };
         Calculator.current = '';
-        Calculator.operator = element.innerHTML;
-        console.log(Calculator.operator);
-        
+        Calculator.operator = element.innerHTML;        
     },
 
-    UpdateScreen: () => {
-        document.getElementById('result').value = Calculator.current;
+    updateDisplay: () => {
+        Calculator.display.value = Calculator.current;
     },
 
-    ClearScreen: () => {
+    clearAll: () => {
         Calculator.current = '';
         Calculator.operator = '';
         Calculator.previous= '';
-        Calculator.UpdateScreen();
+        Calculator.updateDisplay();
     },
 
-    SaveHistory: (prev, result) => {
-
-        let lista_historial = document.getElementById('lista-historial');
+    addToHistory: (prev, result) => {
+        let historyList = document.getElementById('historyList');
         const li = document.createElement('li');    
-        li.textContent = Calculator.previous + ' ' + Calculator.operator + ' ' + prev + ' = ' + result ;
-        //insertar en el HYML
-        lista_historial.appendChild(li);
-
-    }
-}
-
-Calculator.bttn.forEach(element => {
-    element.addEventListener("click", function(e){
-
-       let check = parseInt(element.innerHTML) / 2;
-
-       if(check >= 0 ) {
-            Calculator.current += element.innerHTML;
-            Calculator.UpdateScreen();
-       }
-
-       else if(element.innerHTML === '=') {        
+        li.textContent = `${Calculator.previous} ${Calculator.operator} ${prev} = ${result}` ;
+        historyList.appendChild(li);
         
-            if (Calculator.previous != '' && Calculator.operator != '' && Calculator.current != ''){
-                let prev = Calculator.current;
-                Calculator.current = Calculator.Operation();
-                let result = Calculator.current;
-                Calculator.SaveHistory(prev,result);
-                Calculator.UpdateScreen();
-            }
-            
-       }
+    },
 
-       else if(element.innerHTML === 'C'){
-            Calculator.ClearScreen();
-       }
+    clearHistory: () => { 
+        const lista_historial = document.getElementById('historyList');
+        while (lista_historial.firstChild) {
+            lista_historial.removeChild(lista_historial.firstChild);
+        }
+    }
 
-       else {
-            Calculator.SelectOperation(element);
-        };
-       
+};
 
-       
-    });
+//Run the object
+Calculator.buttons.forEach(button => {
+    button.addEventListener('click', () => Calculator.handleButtonClick(button));
 });
-
-
-
-
-/*
-function seleccionar_numero(numero) {
-    //almacenar numero seleccionado en una variable
-    elemento_actual += numero;
-    actualizar_pantalla();
-}
-
-function seleccionar_operacion(op) {
-    //comprobar que no este vacia las variable antes de utilizar
-    if (elemento_actual === '') return;
-    if (elemento_anterior !== '') {
-        calcular();
-    }
-    //vaciar variables
-    operador = op;
-    elemento_anterior = elemento_actual;
-    elemento_actual = '';
-}
-
-function calcular() {
-    //verificar que no este vacia la variable
-    if (elemento_actual === '' || elemento_anterior === '') return;
-    let resultado;
-    //convirtiendo a float para manejar decimales
-    const elem_ant = parseFloat(elemento_anterior);
-    const elem_act = parseFloat(elemento_actual);
-    
-    switch (operador) {
-        case '+':
-            resultado = elem_ant + elem_act;
-            break;
-        case '-':
-            resultado = elem_ant - elem_act;
-            break;
-        case '*':
-            resultado = elem_ant * elem_act;
-            break;
-        case '/':
-            resultado = elem_ant / elem_act;
-            break;
-        default:
-            return;
-    }
-    //guardar operacion en el historial
-    agregar_a_historial(`${elemento_anterior} ${operador} ${elemento_actual} = ${resultado}`);
-    elemento_actual = resultado.toString();
-    //vaciar variables
-    operador = '';
-    elemento_anterior = '';
-    actualizar_pantalla();
-}
-
-function borrar_pantalla() {
-    //vaciar variables
-    elemento_actual = '';
-    operador = '';
-    elemento_anterior = '';
-    actualizar_pantalla();
-}
-
-function actualizar_pantalla() {
-    document.getElementById('result').value = elemento_actual;
-}
-
-function agregar_a_historial(operacion) {
-    //crear elemento HTML de lista y darle un contenido
-    const li = document.createElement('li');    
-    li.textContent = operacion;
-    //insertar en el HYML
-    lista_historial.appendChild(li);
-}
-*/
